53321bb954b80cff18fb442b288c98cab2cf6ab4
55199789 <leqizheng2-c@my.cityu.edu.hk>
2024-08-20 21:38:27 +0800

    chore: Update ORAM benchmark filter in exp.py

--- benchmarks/exp.py
+++ benchmarks/exp.py
@@ -4,7 +4,7 @@ import json
 
 process = subprocess.Popen(f"(cd ../build && cmake .. && make -j)", shell=True)
 process.wait()
-for n_base in range(8, 26):
+for n_base in range(8, 20):
     n = 2**n_base 
     file_name = f"result/results_omap_{n}.json"
     if os.path.exists(file_name):
@@ -15,6 +15,6 @@ for n_base in range(8, 26):
             except:
                 pass
     process = subprocess.Popen(f"../bin/PathORAMBenchmark  --benchmark_out={file_name} \
-                                    --benchmark_filter=OMapDataFixture*/OMap/{n}/*", shell=True)
+                                    --benchmark_filter=OMapDataFixture.*/OMap/{n}/*", shell=True)
     print(process)
     process.wait()
